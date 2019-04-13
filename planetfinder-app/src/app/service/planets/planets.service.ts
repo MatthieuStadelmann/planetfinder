7dda3091e9c04125127762f636a10ec4a87baad1
Matthieu Stadelmann <matthstadelmann@gmail.com>
2019-04-13 19:33:02 +0200

    Connect planets service with real data

--- planetfinder-app/src/app/service/planets/planets.service.ts
+++ planetfinder-app/src/app/service/planets/planets.service.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@angular/core';
 import { Planet } from '../../models/planet';
-import { MOCK_PLANETS } from '../../mock/mock-planets';
-import { Observable, of } from 'rxjs';
+import {Observable} from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import {map} from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlanetsService {
+  private apiUrl = 'https://swapi.co/api/planets/';
 
-  constructor() { }
-  getPlanets(): Observable<Planet[]> {
-    return of(MOCK_PLANETS);
+  constructor(private http: HttpClient) { }
+  allPlanets(): Observable<Planet[]> {
+    return this.http.get<any>(this.apiUrl)
+      .pipe(
+        map(res => res.results)
+      );
   }
 }
