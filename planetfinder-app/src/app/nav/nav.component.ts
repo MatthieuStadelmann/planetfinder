61b9d65dba268f6b3ae25f4f38cab0355b9f4301
Matthieu Stadelmann <matthstadelmann@gmail.com>
2019-04-13 23:03:49 +0200

    Add searchResults to NavComponent

--- planetfinder-app/src/app/nav/nav.component.ts
+++ planetfinder-app/src/app/nav/nav.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {Subject, Observable} from 'rxjs';
+import {PlanetsService} from '../service/planets/planets.service';
+import {
+  debounceTime, distinctUntilChanged, switchMap
+} from 'rxjs/operators';
+import {Planet} from '../models/planet';
 
 @Component({
   selector: 'app-nav',
@@ -6,10 +12,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
+  planets$: Observable<Planet[]>;
+  private searchTerms = new Subject<string>();
 
-  constructor() { }
+  constructor(private planetsService: PlanetsService) { }
+
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
 
   ngOnInit() {
+    this.planets$ = this.searchTerms.pipe(
+      debounceTime(300),
+
+      distinctUntilChanged(),
+
+      switchMap((term: string) => this.planetsService.searchPlanetByName(term)),
+    );
+
   }
 
 }
