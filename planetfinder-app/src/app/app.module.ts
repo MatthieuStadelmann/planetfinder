5e036c64a01e46687d881e7c1e6372245e9f92e0
Matthieu Stadelmann <matthstadelmann@gmail.com>
2019-04-13 22:11:20 +0200

    Add NavComponent and new icon

--- planetfinder-app/src/app/app.module.ts
+++ planetfinder-app/src/app/app.module.ts
@@ -6,11 +6,15 @@ import { AppComponent } from './app.component';
 import { PlanetsComponent } from './planets/planets.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTableModule, MatPaginatorModule, MatSortModule } from '@angular/material';
+import { HttpClientModule } from '@angular/common/http';
+import { NavComponent } from './nav/nav.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    PlanetsComponent
+    PlanetsComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
@@ -18,7 +22,8 @@ import { MatTableModule, MatPaginatorModule, MatSortModule } from '@angular/mate
     BrowserAnimationsModule,
     MatTableModule,
     MatPaginatorModule,
-    MatSortModule
+    MatSortModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
