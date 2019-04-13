2733fee9a0c084218d29eb40a7f60bcc4aa3c51c
Matthieu Stadelmann <matthstadelmann@gmail.com>
2019-04-13 23:02:38 +0200

    Add SearchPlanetByName to PlanetsService

--- planetfinder-app/src/app/service/planets/planets.service.ts
+++ planetfinder-app/src/app/service/planets/planets.service.ts
@@ -1,15 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Planet } from '../../models/planet';
-import {Observable} from 'rxjs';
+import {Observable, of} from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import {map} from 'rxjs/operators';
+import {catchError, map, tap} from 'rxjs/operators';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlanetsService {
-  private apiUrl = 'https://swapi.co/api/planets/';
+  private apiUrl = 'https://swapi.co/api/planets';
 
   constructor(private http: HttpClient) { }
   allPlanets(): Observable<Planet[]> {
@@ -17,5 +17,24 @@ export class PlanetsService {
       .pipe(
         map(res => res.results)
       );
+  }
+  searchPlanetByName(term: string): Observable<Planet[]> {
+    if (!term.trim()) {
+      return of([]);
+    }
+
+    return this.http.get<any>(`${this.apiUrl}/?search=${term}`).pipe(
+      map(res => res.results),
+      tap(_ => console.log(`found planet matching "${term}"`)),
+      catchError(this.handleError<Planet[]>('searchPlanetByName', []))
+    );
+  }
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      console.error(error);
+      console.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    };
   }
 }
