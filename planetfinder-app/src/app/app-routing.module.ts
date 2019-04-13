f64d2166412bd7c50913d6e97df489c3876e6c54
Matthieu Stadelmann <matthstadelmann@gmail.com>
2019-04-13 15:51:28 +0200

    Update app-routing with new PlanetsComponent

--- planetfinder-app/src/app/app-routing.module.ts
+++ planetfinder-app/src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PlanetsComponent } from './planets/planets.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path: '', component: PlanetsComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
