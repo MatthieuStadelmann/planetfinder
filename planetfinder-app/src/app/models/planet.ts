546486a2fad29bbec502090d586e405fae481910
Matthieu Stadelmann <matthstadelmann@gmail.com>
2019-04-14 16:03:51 +0200

    Refactor Planet interface & mock-planet, add Film Interface, Refactor global styling.

--- planetfinder-app/src/app/models/planet.ts
+++ planetfinder-app/src/app/models/planet.ts
@@ -1,13 +1,13 @@
 export interface Planet {
   name: string;
   diameter: string;
-  rotationPeriod: string;
-  orbitalPeriod: string;
+  rotation_period: string;
+  orbital_period: string;
   gravity: string;
   population: string;
   climate: string;
   terrain: string;
-  surfaceWater: string;
+  surface_water: string;
   residents: Array<string>;
   films: Array<string>;
   url: string;
